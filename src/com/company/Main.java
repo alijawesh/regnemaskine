cd48aaeeb54c0dd5d48341e149ff7cb26177917f
ali <ali.jawesh5@gmail.com>
2020-12-05 12:03:19 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner input = new Scanner(System.in);
+        float firstn;
+        float secoundn;
+        float answer;
+        System.out.println("first number");
+        firstn = input.nextFloat();
+        System.out.println("secound number");
+        secoundn = input.nextFloat();
+        answer = firstn * secoundn;
+        System.out.println("the answer");
+        System.out.println(answer);
+    }
+}
