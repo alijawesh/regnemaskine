9abc55344bbdeb7ddaaa19efe3465f1bc5caea88
ali <ali.jawesh5@gmail.com>
2020-12-05 12:09:02 +0100

    kom2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-//dfgdff
+
     public static void main(String[] args) {
         // write your code here
         Scanner input = new Scanner(System.in);
